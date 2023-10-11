f46236f19519b701635182e72156bb92900a3e35
lakesman <olalekanonauga@gmail.com>
2023-10-11 13:28:20 +0100

    Updated commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <MainLayouts>
     <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-        </div
-  ></MainLayouts>
+      <div
+        class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
+      >
+        <div v-if="products" v-for="product in products.data" :key="product">
+          <ProductComponent :product="product" />
+        </div>
+      </div>
+      index
+    </div></MainLayouts
+  >
 </template>
 
 <script setup>
 import MainLayouts from "~/layouts/MainLayouts.vue";
+
+
+const products =[{}]
 </script>
 
